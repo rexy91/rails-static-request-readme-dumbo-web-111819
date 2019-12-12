e55040c73d83be02239d47d2035e3bb9dd9a1ac0
Rex Ye <rxye1991@gmail.com>
2019-12-12 18:44:29 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+<<<<<<< HEAD
 
     get 'about', to: 'static#about'
     
 
+=======
+  
+  get 'about', to: 'static#about'
+>>>>>>> 4a3b485d7205ea9c70b48818a7c50d801a198873
 end
